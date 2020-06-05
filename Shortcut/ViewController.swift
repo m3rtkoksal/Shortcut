e9541b55550a5eaf2db11c9f8b6ebd3e9fd36c72
MK <mertkoksal88@gmail.com>
2020-06-05 13:12:24 +0300

    Initial Commit

---
+++ Shortcut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shortcut
+//
+//  Created by Mert Köksal on 5.06.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
